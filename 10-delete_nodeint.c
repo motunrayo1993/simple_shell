aac4333748fcfc47e5d99f29f4e91b42088e437d
Sani Abuh <101403891+Saniabuh@users.noreply.github.com>
2022-06-15 09:35:04 +0100

    shell

---
+++ 10-delete_nodeint.c
@@ -0,0 +1,47 @@
+#include "shell.h"
+
+/**
+ * delete_nodeint_at_index - delete a node at a given position
+ * @head: pointer to head pointer of linked list
+ * @index: index to delete node
+ * Return: 1 if succeeded, or -1 if failed
+ */
+
+int delete_nodeint_at_index(list_t **head, unsigned int index)
+{
+
+	unsigned int i = 0;
+	list_t *tmp, *tmp2;
+
+	/* account for empty list */
+	if (*head == NULL)
+		return (-1);
+
+	tmp = *head;
+
+	/* account for deleting beginning node */
+	if (index == 0)
+	{
+		*head = tmp->next;
+		free(tmp);
+		return (1);
+	}
+
+	/* iterate tmp to idx prior to idx we want to delete */
+	while (i < (index - 1) && tmp != NULL)
+	{
+		tmp = tmp->next;
+		i++;
+	}
+
+	/* account for idx out of range: don't delete and return */
+	if (i != (index - 1) || tmp->next == NULL)
+		return (-1);
+
+	/* link prior idx before delete */
+	tmp2 = tmp->next;
+	tmp->next = (tmp->next)->next;
+	free(tmp2);
+
+	return (1);
+}
